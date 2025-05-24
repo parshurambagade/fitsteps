46ebdc9b75028e25bba6484de93171fa070565b5
Parshuram Bagade <parshurambagade855@gmail.com>
2025-05-24 20:44:28 +0530

    feat: bottom tab bars for home, history and challenges screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,73 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { BlurView } from "expo-blur";
+import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
 import { GluestackUIProvider } from "./components/ui/gluestack-ui-provider";
-
 export default function RootLayout() {
   return (
     <GluestackUIProvider>
-      <Stack />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#2F80ED",
+          tabBarStyle: {
+            position: "absolute",
+            flex: 1,
+            paddingTop: 8,
+          },
+          sceneStyle: {
+            backgroundColor: "#0A0E23",
+          },
+          tabBarBackground: () => (
+            <BlurView
+              experimentalBlurMethod={"dimezisBlurView"}
+              intensity={25}
+              className="bg-custom-bgCard overflow-hidden"
+              style={{ ...StyleSheet.absoluteFillObject }}
+            />
+          ),
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "home-sharp" : "home-outline"}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="history"
+          options={{
+            title: "History",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "podium-sharp" : "podium-outline"}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="challenges"
+          options={{
+            title: "Challenges",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "trophy-sharp" : "trophy-outline"}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+      </Tabs>
     </GluestackUIProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center">
-      <Text className="text-custom-primary text-center">FitSteps</Text>
+    <View className={`flex-1 justify-center`}>
+      <Text className="text-custom-primary text-center my-20">FitSteps</Text>
     </View>
   );
 }
