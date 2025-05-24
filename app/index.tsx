8e1ce357d40350a1b7de83409514402a7692a238
Parshuram Bagade <parshurambagade855@gmail.com>
2025-05-24 19:34:43 +0530

    Add: Initial setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@/app/components/ui/text";
+import React from "react";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center">
+      <Text className="text-custom-primary text-center">FitSteps</Text>
+    </View>
+  );
+}
